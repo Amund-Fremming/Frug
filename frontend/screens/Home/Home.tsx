ff40d0b4f03dd6891e02a8d0a48d7c41292902aa
amund-fremming <amund.fremming@gmail.com>
2024-01-13 12:54:51 +0100

    Working on Home page

--- frontend/screens/Home/Home.tsx
+++ frontend/screens/Home/Home.tsx
@@ -1,19 +1,39 @@
-import { Text, View } from "react-native";
-import { styles } from "./HomeStyles";
+import { Text, View, Image } from "react-native";
+import { styles, imageStyle } from "./HomeStyles";
 
 export default function Home() {
   const planetOne = require("../../assets/images/planets/planetOne.png");
   const planetTwo = require("../../assets/images/planets/planetTwo.png");
   const planetThree = require("../../assets/images/planets/planetThree.png");
 
+  const mascot = require("../../assets/images/mascot.png");
+
   return (
     <View style={styles.container}>
       <View style={styles.layerOneCircle}>
         <View style={styles.layerTwoCircle}>
           <View style={styles.layerThreeCircle}>
-            <Text>X</Text>
+            <Text>Hey</Text>
           </View>
         </View>
+      </View>
+
+      {/* Absolute pos planets */}
+      <View style={styles.imageContainer}>
+        <View style={styles.planetOne}>
+          <Image source={planetOne} style={imageStyle.planetOne} />
+        </View>
+        <View style={styles.planetTwo}>
+          <Image source={planetTwo} style={imageStyle.planetTwo} />
+        </View>
+        <View style={styles.planetThree}>
+          <Image source={planetThree} style={imageStyle.planetThree} />
+        </View>
+      </View>
+
+      {/* Absolute pos mascot */}
+      <View style={styles.mascotContainer}>
+        <Image source={mascot} style={imageStyle.mascot} />
       </View>
     </View>
   );
--- frontend/screens/Home/HomeStyles.js
+++ frontend/screens/Home/HomeStyles.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     width: "100%",
     height: "100%",
@@ -42,4 +42,72 @@ export const styles = StyleSheet.create({
     borderRadius: "3000%",
     backgroundColor: "#A66444",
   },
+
+  imageContainer: {
+    width: "100%",
+    position: "absolute",
+    top: 50,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  planetOne: {
+    width: 150,
+    height: 150,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  planetTwo: {
+    width: 150,
+    height: 150,
+    marginBottom: 60,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  planetThree: {
+    width: 150,
+    height: 150,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  mascotContainer: {
+    position: "absolute",
+    top: 200,
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
+
+const imageStyle = StyleSheet.create({
+  planetOne: {
+    width: 110,
+    height: 110,
+  },
+
+  planetTwo: {
+    width: 100,
+    height: 100,
+  },
+
+  planetThree: {
+    width: 80,
+    height: 80,
+  },
+
+  mascot: {
+    width: 330,
+    height: 330,
+  },
+});
+
+export { styles, imageStyle };
